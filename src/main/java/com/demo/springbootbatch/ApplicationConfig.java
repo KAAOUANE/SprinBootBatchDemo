b749124e7bfc244add496cdc0597ed5f6cc00ef5
mounir <mounirlyon@gmail.com>
2018-04-03 21:36:47 +0200

    add multiDatasource

--- src/main/java/com/demo/springbootbatch/ApplicationConfig.java
+++ src/main/java/com/demo/springbootbatch/ApplicationConfig.java
@@ -3,7 +3,13 @@ package com.demo.springbootbatch;
 import org.springframework.batch.core.configuration.annotation.EnableBatchProcessing;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.boot.jdbc.DataSourceBuilder;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ImportResource;
+import org.springframework.context.annotation.Primary;
+
+import javax.sql.DataSource;
 
 /**
  * Created by admin on 03/04/2018.
@@ -11,8 +17,22 @@ import org.springframework.context.annotation.ImportResource;
 @SpringBootApplication
 @EnableBatchProcessing
 @ImportResource("classpath:batchjob.xml")
+//@Configuration //pour les data sources
 public class ApplicationConfig {
     public static void main(String[] args) {
         SpringApplication.run(ApplicationConfig.class, args);
+    }
+
+    @Bean
+    @Primary
+    @ConfigurationProperties(prefix = "datasource.primary")
+    public DataSource primaryDataSource() {
+        return DataSourceBuilder.create().build();
+    }
+
+    @Bean
+    @ConfigurationProperties(prefix = "datasource.secondary")
+    public DataSource secondaryDataSource() {
+        return DataSourceBuilder.create().build();
     }
 }
